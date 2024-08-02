398b08f2922c603157b24bb606173afd7bde0f6a
Derrick Koko <kokoderrick3@gmail.com>
2024-08-02 22:21:05 +0100

    "Updated string literals to use double quotes instead of single quotes."

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from pyfingerprint.pyfingerprint import PyFingerprint
 import time
 
-def enroll_fingerprint(device_path='/dev/tty.S818', baud_rate=57600):
+
+def enroll_fingerprint(device_path="/dev/tty.S818", baud_rate=57600):
     try:
         print(f"Trying to connect to {device_path} with baud rate {baud_rate}...")
         f = PyFingerprint(device_path, baud_rate, 0xFFFFFFFF, 0x00000000)
 
         if not f.verifyPassword():
-            raise ValueError('The given fingerprint sensor password is wrong!')
+            raise ValueError("The given fingerprint sensor password is wrong!")
 
     except Exception as e:
-        print('The fingerprint sensor could not be initialized!')
-        print('Exception message:', e)
+        print("The fingerprint sensor could not be initialized!")
+        print("Exception message:", e)
         return
 
     try:
-        print('Waiting for finger...')
+        print("Waiting for finger...")
         while not f.readImage():
             pass
 
@@ -24,29 +25,30 @@ def enroll_fingerprint(device_path='/dev/tty.S818', baud_rate=57600):
         positionNumber = result[0]
 
         if positionNumber >= 0:
-            print('Fingerprint already exists at position #', positionNumber)
+            print("Fingerprint already exists at position #", positionNumber)
             return
 
-        print('Remove finger...')
+        print("Remove finger...")
         f.waitForFingerUp()
-        print('Waiting for the same finger again...')
+        print("Waiting for the same finger again...")
         while not f.readImage():
             pass
 
         f.convertImage(0x02)
         if f.compareCharacteristics() == 0:
-            raise Exception('Fingers do not match')
+            raise Exception("Fingers do not match")
 
         f.createTemplate()
         positionNumber = f.storeTemplate()
-        print('Fingerprint enrolled successfully!')
-        print('New template position #', positionNumber)
+        print("Fingerprint enrolled successfully!")
+        print("New template position #", positionNumber)
 
     except Exception as e:
-        print('Operation failed!')
-        print('Exception message:', e)
+        print("Operation failed!")
+        print("Exception message:", e)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Try with the default baud rate
     enroll_fingerprint()
 
